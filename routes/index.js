8aef025c0d5d7d71569e38dccb5d350e04a3b85b
KayGV25 <khuongvudang25@gmail.com>
2024-05-11 15:40:43 +0700

    update for deploy

--- routes/index.js
+++ routes/index.js
@@ -129,7 +129,6 @@ async function Update(User, Type){
     //Read rows from spreadsheet
   
     const cord = User.Cid[0];
-    console.log(cord);
     await googleSheets.spreadsheets.values.update({
         auth,
         spreadsheetId,
